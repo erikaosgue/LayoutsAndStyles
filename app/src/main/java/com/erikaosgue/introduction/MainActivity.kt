ad5adbfe4c9fe527f7a9c6c45c0050933c8d7773
erikaosgue <1476@holbertonschool.com>
2021-03-11 14:38:46 -0500

    Add Toast for the App

--- app/src/main/java/com/erikaosgue/introduction/MainActivity.kt
+++ app/src/main/java/com/erikaosgue/introduction/MainActivity.kt
@@ -3,6 +3,7 @@ package com.erikaosgue.introduction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.erikaosgue.introduction.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         var enterText = enterNameEdt.text
 
         showNameButton.setOnClickListener{ resultId.text = enterText}
+
+        // Appears the message Hello there, for a short time
+        Toast.makeText(this, "Hello there", Toast.LENGTH_LONG)
+            .show()
+
 
     }
 }
